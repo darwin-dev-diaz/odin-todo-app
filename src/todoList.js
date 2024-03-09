a3ce0fa208cf551cc4a66f8132cb6e9bd65fd61f
Darwin Diaz <darwin.diaz.ut@gmail.com>
2024-03-08 18:31:11 -0700

    Create taskList and createTask function

--- src/todoList.js
+++ src/todoList.js
@@ -1,2 +1,12 @@
 // this file will be where the logic of the todo list is stored. It will have scripts to create the tasks and group them into projects
 
+const taskList = [];
+
+function createTask (title, description, dueDate, priority, completed = false, project = none){
+    return {title, description, dueDate, priority, completed, project};
+}
+
+taskList.push(createTask('Finish homework', 'nothing', '03-08-2024', 'important', false, 'school'));
+taskList.push(createTask('Workout', 'Do 100 push ups today', '03-09-2024', 'important', false, 'health'));
+
+console.log(taskList);
